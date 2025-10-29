10b6c04a74bb86a9b42b5409f9ff6286a155ae64
Tyler Maschoff <Tyler.Maschoff@fbfs.com>
2025-10-29 07:22:32 -0500

    datafast script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Providers from "./providers"
 import { LayoutContent } from "@/components/layout-content"
+import Script from "next/script";
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +20,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <head>
+        <Script
+          data-website-id="dfid_690205949903c6f4d82ca553"
+          data-domain="oddsmash.io"
+          src="https://datafa.st/js/script.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
