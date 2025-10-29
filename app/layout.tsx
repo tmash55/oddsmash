01ef865012136d7e1fd4b821b2037c511ad17e6c
Tyler Maschoff <Tyler.Maschoff@fbfs.com>
2025-10-29 07:18:53 -0500

    datafast script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { Inter } from "next/font/google"
+import localFont from "next/font/local"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
@@ -8,6 +9,7 @@ import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Providers from "./providers"
 import { LayoutContent } from "@/components/layout-content"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <head>
+            <Script
+              defer
+              data-website-id="dfid_vBeGKA8yYNHfjoHXAitoQ"
+              data-domain="oddsmash.io"
+              src="https://datafa.st/js/script.js"/>
+        </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
